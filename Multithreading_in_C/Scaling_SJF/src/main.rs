a9ee9d8bc5e40d71713c76834517dbceff623088
GabrielCoelho <gbcoe@outlook.com.br>
2024-10-01 11:13:03 -0300

    feat(Random Proccesses): Populating the vector of structs

--- Multithreading_in_C/Scaling_SJF/src/main.rs
+++ Multithreading_in_C/Scaling_SJF/src/main.rs
@@ -1,10 +1,28 @@
 // Copyright (c) 2024 Gabriel Coelho Soares. All Rights Reserved.
 mod queue;
 use queue::*;
+use random_string::generate;
 
 fn main() {
     let mut total_exec_time: i32;
     let mut total_await_time: i32;
+    let mut i = 0;
     let mut proc: Vec<Proccess> = Vec::new();
     println!("SJF Scaling in Rust");
+
+    loop {
+        proc.push(Proccess::new(
+            generate(5, random_string::charsets::ALPHA_LOWER),
+            fastrand::u8(0..20),
+            fastrand::u32(1..10),
+        ));
+        i += 1;
+        if i == 5 {
+            break;
+        }
+    }
+
+    for val in proc {
+        println!("{}\n{}\n{}\n", val.name, val.entry_time, val.execution_time);
+    }
 }
