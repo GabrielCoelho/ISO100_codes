3e61079fa87a019bef2b613322fe6335aaddd64c
GabrielCoelho <gbcoe@outlook.com.br>
2024-11-23 20:42:50 -0300

    Adicionando a base do código

--- assignmentmemorypagination/src/main.rs
+++ assignmentmemorypagination/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Memory Pagination\n\n1. FIFO\n2. Optimal\n3. Exit");
+        let mut num: String = String::new();
+        io::stdin()
+            .read_line(&mut num)
+            .expect("Couldn't read a number");
+        let num: i32 = num.trim().parse().expect("Error while parsing");
+        match num {
+            1 => fifo(),
+            2 => optimal(),
+            3 => {
+                println!("Exiting the program");
+                break;
+            }
+            _ => println!("Couldn't find this option, try again"),
+        }
+    }
 }
+
+fn fifo() {}
+
+fn optimal() {}
